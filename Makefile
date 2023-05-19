d16410c9964dc203c48dd6bc1f27b33ee5f86a94
W7R7R7U7S <gtdm1912@gmail.com>
2023-05-19 10:08:32 +0700

    Create function wtime and random in main.c

--- Makefile
+++ Makefile
@@ -8,11 +8,9 @@ run:
 	gcc -shared -o libfsdyn.so bubble_sort.o quick_sort.o
 	gcc -c main.c
 	gcc main.o -L. -lfsdyn -o resultdyn
-	gcc -static-libgo main.o -L. -lfs  -o rez1
 
 clean:	
 	rm main.o
 	rm bubble_sort.o
 	rm quick_sort.o
 	rm libfsdyn.so
-	rm resultdyn
--- main.c
+++ main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dlfcn.h>
+#include <time.h>
+#include <sys/time.h>
 #include "sort_lib.h"
+
+#define SIZE_1 28
+#define SIZE_2 216
+#define STEP 4
+
+double wtime()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}
+
+int getrand(int min, int max)
+{
+    return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
+}
+
 int main() {
     // Загрузка библиотеки
     void* lib = dlopen("./libfsdyn.so", RTLD_LAZY);
     if (!lib) {
         printf("Ошибка загрузки библиотеки: %s\n", dlerror());
-        return 1;    }
+        return 1;
+    }
+    
     // Получение указателей на функции сортировки
     void (*bubble_sort)(int[], int) = dlsym(lib, "bubble_sort");
     void (*quick_sort)(int[], int, int) = dlsym(lib, "quick_sort");
@@ -15,29 +37,40 @@ int main() {
         dlclose(lib);
         return 1;
     }
-    // Пример использования функций сортировки
-    int arr[] = {5, 2, 8, 1, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Исходный массив: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    
+    // Инициализация генератора случайных чисел
+    srand(time(NULL));
+    
+    printf("Count\t      Bubble Sort    Quick Sort\n");
+
+    for (int i = SIZE_1; i <= SIZE_2; i += STEP) {
+        int* arr = (int*)malloc(i * sizeof(int));
+
+        if (i == 216){i = 215;}
+        printf("%d\t", i);
+        for (int j = 0; j < i; j++) {
+            arr[j] = getrand(28, 215);
+        }
+
+        double start, end;
+
+        start = wtime();
+        bubble_sort(arr, i);
+        end = wtime() - start;
+        printf("%15.7lf", end);
+
+        start = wtime();
+        quick_sort(arr, 0, i - 1);
+        end = wtime() - start;
+        printf("%15.7lf", end);
+
+        printf("\n");
+
+        free(arr);
     }
-    printf("\n");
-    // Сортировка методом пузырька
-    bubble_sort(arr, n);
-    printf("Отсортированный массив (метод пузырька): ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    // Сортировка быстрым методом
-    quick_sort(arr, 0, n - 1);
-    printf("Отсортированный массив (быстрая сортировка): ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+
     // Выгрузка библиотеки
     dlclose(lib);
+
     return 0;
 }
