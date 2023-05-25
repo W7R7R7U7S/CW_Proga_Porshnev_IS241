f9dcf7e8009e1198f937d865377a1b2bf012080e
Maxim Porshnev <gtdm1912@gmail.com>
2023-05-25 23:56:55 +0700

    Fix main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import random
-
-# Генерируем случайные числа от 256 до 32000
 numbers = [random.randint(1, 10000000000) for _ in range(32768)]
-
-# Сортируем числа по возрастанию
-
-
-# Создаем новый файл и записываем отсортированные числа в него
 with open('sorted_numbers_random.txt', 'w') as file:
     for number in numbers:
         file.write(str(number) + '\n')
